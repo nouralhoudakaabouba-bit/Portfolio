13831dfe8d1134860d69e0cef5e4bd3fc5534115
Nouralhouda <nouralhoudakaabouba@gmail.com>
2026-05-09 18:33:45 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".text", {
+    strings: [
+        "Telecom Engineer",
+        "Junior Product Owner",
+        "RAN Engineer",
+        "Frontend Learner"
+    ],
+
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
