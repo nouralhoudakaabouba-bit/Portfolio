62992cd138182297d0319b415be29fc0be1dfd43
Nouralhouda Kaabouba <nouralhoudakaabouba@gmail.com>
2026-05-09 20:43:30 +0200

    changes

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var typed = new Typed(".text", {
     strings: [
         "Telecom Engineer",
@@ -10,18 +9,4 @@ var typed = new Typed(".text", {
     backSpeed: 100,
     backDelay: 1000,
     loop: true
-=======
-var typed = new Typed(".text", {
-    strings: [
-        "Telecom Engineer",
-        "Junior Product Owner",
-        "RAN Engineer",
-        "Frontend Learner"
-    ],
-
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
->>>>>>> e6d1000fe173f38bc5f5b9462e678f2fd4ede1f5
 });
--- readme.md
+++ readme.md
@@ -1,5 +1,2 @@
 <<<<<<< HEAD
 # Personallll-Portfolio-Website-using-HTML-CSS-JavaScript
-=======
-# MY-Personal-Portfolio-Website-using-HTML-CSS-JavaScript
->>>>>>> e6d1000fe173f38bc5f5b9462e678f2fd4ede1f5
