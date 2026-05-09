f4580545b2e83041dc928294620b45741d5c555d
Nouralhouda Kaabouba <nouralhoudakaabouba@gmail.com>
2026-05-09 20:11:39 +0200

    initial portfolio setup

---
+++ main.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text", {
+    strings: [
+        "Telecom Engineer",
+        "Junior Product Owner",
+        "RAN Engineer"
+    ],
+
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
